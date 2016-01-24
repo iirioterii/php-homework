af62b518cb96c1ad9fe23d2d5a50accbf7a88deb
iirioterii <yuriyreva@gmail.com>
2016-01-24 15:14:14 +0200

    class del, and require too

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 
-use MyTest\Test;
 use Src\NewClass;
 use PHP_Group\Bogdan;
 
 require_once('vendor/autoload.php');
-
-$test = new Test();
-$test->hello();
 
 $newClass = new NewClass();
 $newClass->test();
